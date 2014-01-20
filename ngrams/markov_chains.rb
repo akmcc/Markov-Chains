d0027114b37f7de37c05dc7f533e8a8c740a6041
Ashton McCrate <a.mccrate@gmail.com>
2014-01-19 21:45:38 -0800

    refactor

--- ngrams/markov_chains.rb
+++ ngrams/markov_chains.rb
@@ -1,85 +1,73 @@
 require 'treat'
 
-class Markov
+class TextGenerator
 
   include Treat::Core::DSL
 
-  # type can be 'directory' or 'file'
-  def tokenize(type, file_name)  
-    if type == 'directory'
-      story = collection(file_name)
-    elsif type == 'file'
-      story = document(file_name)
-    end
-    story.apply(:chunk, :segment, :tokenize, :category)
-    @tokenized_sentences = []
-    story.sentences.each do |sent|
-      @tokenized_sentences << sent.to_a
-    end
-    @tokenized_sentences
+  def initialize(training_file)
+    text = document(training_file).apply(:chunk, :segment, :tokenize)
+    @sentences = text.sentences.map {|sent| sent.to_a }
   end
 
-  def count_bigrams(tokenized_stories)
-    @count = {}
-    tokenized_stories.each do |sent|
+  def bigrams(sentences)
+    count = Hash.new {|h, k| h[k] = Hash.new {|i, j| i[j] = 0 } }
+    sentences.each do |sent|
       sent = ["None"] + sent 
       # creates the bigrams by zipping the sentenece together w/ subset of sentence
-      sent.zip(sent[1..-1]).each do |current, on_deck|
-        @count[current] ||= {}
-        @count[current][on_deck] ||= 0
-        @count[current][on_deck] += 1
+      sent.zip(sent[1..-1]).each do |word, next_word|
+        count[word][next_word] += 1
       end
     end
-    @count
+    count
   end
 
   #takes the count hash, and returns new hash with probabilities
-  def find_probability(counted_bigrams)
-    @probability = {}
-    counted_bigrams.each do |key, count_hash|
-      count_hash.each do |inner_key, value|
-        @probability[key] ||= {}
+  def probabilities(bigrams)
+    probability = Hash.new {|h, k| h[k] = {} }
+    bigrams.each do |word, counts|
+      counts.each do |next_word, freq|
         #set probability of each word by diving it's count by the count of all words in that hash
-        @probability[key][inner_key] = (value.to_f / counted_bigrams[key].values.inject(0){|a,b| a + b}.to_f)
+        probability[word][next_word] = (freq.to_f / bigrams[word].values.inject(0){|a,b| a + b}.to_f)
       end
     end
-    @probability
+    probability
   end
 
-  def sample_word(probability_hash)
+  #takes the inner hash of the probabilities which includes just the follow words and their probability
+  def sample_word(follow_words)
     score = rand(0)
-    while true
-      probability_hash.each do |word, prob|
-        if score < prob
-          return word
-        else
-          score -= prob
-        end
+    follow_words.each do |word, prob|
+      if score < prob
+        return word
+      else
+        score -= prob
       end
     end
   end
 
-  def sample_sentence(probability_hash)
-    current = "None"
+  def sample_sentence(probabilities)
     sentence = ""
-    next_word = sample_word(probability_hash[current])
-    until next_word == nil
-      if next_word.match(/[\,\.\;\:\-\!\?]/)
-        sentence << next_word
+    #"None" is the marker for the beginning of sentences
+    current_word = sample_word(probabilities["None"]) 
+    until current_word == nil
+      if current_word.match(/[[:punct:]]/)
+        sentence << current_word
       else
-        sentence << " #{next_word}"
+        sentence << " " + current_word
       end
-      current = next_word
-      next_word = sample_word(probability_hash[current])
+      current_word = sample_word(probabilities[current_word])
     end
     sentence
   end
+
+  def speak
+    puts sample_sentence(probabilities(bigrams(@sentences)))
+  end
 end
 
+metamor = TextGenerator.new('texts/metamorphosis.txt')
+metamor.speak
 
-#this takes forever
-markov = Markov.new
-token_sents = markov.tokenize('directory', 'texts')
-probs = markov.find_probability(markov.count_bigrams(token_sents))
-puts markov.sample_sentence(probs)
+
+
 
